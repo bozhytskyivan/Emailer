b2013bd7de6a5a8c9a74505d57443ace656087b4
ivbo1115 <bojivan.pro@gmail.com>
2017-04-04 11:22:30 +0300

    Refactored project structure. Confugured file logging

--- src/main/java/com/covain/projects/emailer/ui/LoginForm.java
+++ src/main/java/com/covain/projects/emailer/ui/LoginForm.java
@@ -1,10 +1,16 @@
 package com.covain.projects.emailer.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginForm extends JFrame {
+
+    private static Logger LOG = LoggerFactory.getLogger(LoginForm.class);
+
     private JTextField emailTextField;
     private JButton loginButton;
     private JPasswordField passwordTextField;
@@ -16,7 +22,8 @@ public class LoginForm extends JFrame {
 
     public LoginForm() {
         super("EmailSender");
-
+        LOG.info("Entered LoginForm");
+        System.out.println(LOG.isDebugEnabled());
         mainForm = new MainForm(this);
         setContentPane(basePanel);
         pack();
@@ -26,7 +33,7 @@ public class LoginForm extends JFrame {
 
         setDefaultLookAndFeelDecorated(true);
         loginButton.addActionListener(new ActionListener() {
-            @Override
+
             public void actionPerformed(ActionEvent e) {
                 if (passwordTextField.getPassword().length == 0) {
                     new ExceptionDialog(LoginForm.this, "Password must be filled");
